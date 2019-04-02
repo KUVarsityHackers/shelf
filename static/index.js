00d1760e65347a1f5395e8e5b3dd9967e8a3075b
czfloyd <floyd.c23@gmail.com>
2019-04-02 17:25:40 -0500

    Set up call to post a listing

--- static/index.js
+++ static/index.js
@@ -10,3 +10,19 @@ function onSubmit() {
 
   return false;
 }
+
+function submitListing() {
+
+  let userID = document.getElementById("userName").value;
+  let email = document.getElementById("email").value;
+  let isbn = document.getElementById("isbn").value;
+
+  const url = "/listing";
+  $.post(url, {
+    json_string: JSON.stringify({
+      user: userID,
+      email: email,
+      isbn: isbn
+    })
+  })
+}
