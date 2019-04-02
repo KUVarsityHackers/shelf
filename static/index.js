cf26655edf20d336b20b2bd9563cf4679233b10a
czfloyd <floyd.c23@gmail.com>
2019-04-02 16:49:04 -0500

    Reformatted existing front-end code

--- static/index.js
+++ static/index.js
@@ -1,11 +1,11 @@
-function onSubmit(){
+function onSubmit() {
 
   let userID = document.getElementById("userName").value;
   let password = document.getElementById("password").value;
-  
-  const url='/login';
+
+  const url = '/login';
   $.post(url, {
-    json_string: JSON.stringify({ userID: userID, password:password})
+    json_string: JSON.stringify({ userID: userID, password: password })
   });
 
   return false;
