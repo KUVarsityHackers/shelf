2af2c32675136d355465a28afaa231c6070cdd74
Michael Svoren <michael.svoren@gmail.com>
2019-04-06 14:20:12 -0500

    modularize bookInfo

--- bookInfo.py
+++ bookInfo.py
@@ -1,13 +1,17 @@
 #https://www.googleapis.com/books/v1/volumes?q=isbn:9781119259756
 #https://www.geeksforgeeks.org/get-post-requests-using-python/
 
+
 import requests
 import json
-#get isbn from front end
-isbn = str(9781101980132)
-url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
-req = requests.get(url = url)
-jsonFile = req.json()
+
+def getBookInfo(isbnInput):
+    #get isbn from front end
+    isbn = str(9781101980132)
+    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
+    req = requests.get(url = url)
+    jsonFile = req.json()
+    return jsonFile
 
 bookInfo = jsonFile['items'][0]['volumeInfo']
 title = bookInfo['title']
