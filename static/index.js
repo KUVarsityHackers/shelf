f033ba6e4810e7beba602c2f4d25eda49f636b74
czfloyd <floyd.c23@gmail.com>
2019-04-06 15:22:01 -0500

    added search javascript

--- static/index.js
+++ static/index.js
@@ -11,7 +11,7 @@ function onSubmit() {
   return false;
 }
 
-function submitListing() {
+function putOnShelf() {
 
   let userID = document.getElementById("userName").value;
   let email = document.getElementById("email").value;
@@ -23,6 +23,22 @@ function submitListing() {
       user: userID,
       email: email,
       isbn: isbn
+    })
+  })
+}
+
+function searchShelf() {
+
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("borrowISBN").value;
+
+  const url = "/search";
+  let response = $.post(url, {
+    json_string: JSON.stringify({
+      title: title? title : "null",
+      author: author? author : "null",
+      isbn: isbn? isbn : "null"
     })
   })
 }
