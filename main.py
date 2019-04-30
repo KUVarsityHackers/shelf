bbbe8e4c2e29e2518d27e5fd5e08de6566bcbf0d
Michael Svoren <michael.svoren@gmail.com>
2019-04-30 14:37:25 -0500

    Revert "Revert "remove dead code""
    This reverts commit c6c79afc607164f2bdbcd28df19a6a5dbe0543a0.

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def listing():
     
     try:
         sourApple = getBookInfo(isbn)
-        title =  sourApple[0]
+        title =  (sourApple[0]).lower()
         publishedDate = sourApple[1]
     except:
         return ("Could not find match for ISBN.")
@@ -126,29 +126,30 @@ def search():
     d = json.loads(json_acceptable_string)
     
     isbn = d['isbn']
-    title = d['title']
+    title = (d['title']).lower()
     latitude = d['latitude']
     longitude = d['longitude']
     searchRadius = (d['radius'])
 
     searchBy = d['searchBy']
-    print(searchBy)
+    
     try:
         obj = []
         if(searchBy == "Title"):
             listingArr = []
             owners = []
-            query= db.collection(u'books').where(u'title',u'==',title)
-            isbnFromTitle = query.get()
+            query = db.collection(u'books').where(u'title',u'==',title)
+            isbnFromTitle = query.stream()
+            
             for listing in isbnFromTitle:
                 listingArr.append(listing.id)
             for ISBN in listingArr:
-                owners.append(db.collection(u'books').document(ISBN).collection(u'owner').get())
+                owners.append(db.collection(u'books').document(ISBN).collection(u'owner').stream())
                 for docs in owners:
                     for doc in docs:
                         obj.append(doc.to_dict())
         else:
-            docs = db.collection(u'books').document(isbn).collection(u'owner').get()
+            docs = db.collection(u'books').document(isbn).collection(u'owner').stream()
             for doc in docs:
                 obj.append(doc.to_dict())
         
@@ -156,30 +157,30 @@ def search():
         return("Nothing was found.")
     
     #only return responses within user specified search radius
-    newObj = []
-    for person in obj:
-        #calculate distance
-        latFound = person["latitude"]
-        lonFound = person["longitude"]
-                #adapted from https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
-        #approximate radius of earth in km
-        R = 6373.0
-        lat1 = radians(latFound)
-        lon1 = radians(lonFound)
-        lat2 = radians(latitude)
-        lon2 = radians(longitude)
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        distance = R * c
-        if (distance < float(searchRadius)):
-            temp = []
-            temp.append(person["email"])
-            temp.append(distance)
-            newObj.append(temp)
+    # newObj = []
+    # for person in obj:
+    #     #calculate distance
+    #     latFound = person["latitude"]
+    #     lonFound = person["longitude"]
+    #             #adapted from https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
+    #     #approximate radius of earth in km
+    #     R = 6373.0
+    #     lat1 = radians(latFound)
+    #     lon1 = radians(lonFound)
+    #     lat2 = radians(latitude)
+    #     lon2 = radians(longitude)
+    #     dlon = lon2 - lon1
+    #     dlat = lat2 - lat1
+    #     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    #     c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    #     distance = R * c
+    #     if (distance < float(searchRadius)):
+    #         temp = []
+    #         temp.append(person["email"])
+    #         temp.append(distance)
+    #         newObj.append(temp)
 
-    return Response(json.dumps(newObj),  mimetype='application/json')
+    return Response(json.dumps(obj),  mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
